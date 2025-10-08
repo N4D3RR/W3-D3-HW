74870b075b6923d3f3cfe48233e61e3dc473f616
NAD3R <naderdeghaili@gmail.com>
2025-10-08 16:55:27 +0200

    completed exercise

---
+++ scripts/todo.js
@@ -0,0 +1,48 @@
+// il form
+const addForm = document.getElementById("addTaskForm")
+// bottone che apre il form
+const addButton = document.getElementById("addTaskBtn")
+// la lista
+const addedTasks = document.getElementById("addedTasks")
+
+addForm.style.display = "none"
+
+// submit del form
+const formSubmit = function (e) {
+  e.preventDefault()
+
+  // input
+  const taskInput = document.getElementById("titolo")
+  const noteInput = document.getElementById("note")
+  const dataInput = document.getElementById("dataOra")
+
+  const valoreTask = taskInput.value
+  const valoreNote = noteInput.value
+  const valoreData = dataInput.value
+
+  console.log(valoreTask)
+  console.log(valoreNote)
+  console.log(valoreData)
+
+  const li = document.createElement("li")
+  li.textContent = `${valoreTask}${valoreNote ? " - Note: " + valoreNote : ""}${
+    valoreData ? " - Entro: " + valoreData : ""
+  }
+  `
+
+  addedTasks.appendChild(li)
+
+  addForm.reset()
+
+  li.addEventListener("click", () => li.classList.toggle("completed"))
+}
+
+//mostra/nascondi input
+addForm.addEventListener("submit", formSubmit)
+addButton.addEventListener("click", () => {
+  if (addForm.style.display === "none") {
+    addForm.style.display = "block"
+  } else {
+    addForm.style.display = "none"
+  }
+})
